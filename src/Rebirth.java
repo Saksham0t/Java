e3b258b54130427594eee83a348f1b4009569f9a
Saksham <sakshamgupta1279@gmail.com>
2023-06-29 00:06:23 +0530

    updated the gitignore file

---
+++ src/Rebirth.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+public class Rebirth {
+    public static void main(String[] args){
+//        int a = 8;
+//        System.out.println(a);
+//        Scanner sc = new Scanner(System.in);
+//        int b = sc.nextInt();
+//        int sum=a+b;
+//        System.out.println("sum:"+sum);
+//        if(a>b){
+//            System.out.println("a is greater");
+//        }else if(a==b){
+//            System.out.println("a is equal to b");
+//        }
+//        else{
+//            System.out.println("b is greater");
+//            int diff= b-a;
+//            System.out.println("differnce:"+ diff);
+//        }
+
+//        int j=0;
+//        while(j<4){
+//            System.out.println(j);
+//            j++;
+//        }
+
+//        for (int i=0; i<9; i++){
+//            if(i%2 != 0){
+//                System.out.println(i);
+//            }
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter no.:");
+//        int x = sc.nextInt();
+//        for(int i=1; i<=10; i++){
+//            System.out.println(x+"X"+i+"="+x*i);
+//        }
+
+//        int a = 5;
+//        int b = 2;
+//        int c = 15;
+//        if(a<b && b<c) {
+//            System.out.println("yes");
+//        }else if(a<b || b<c){
+//            System.out.println("no");
+//        }else{
+//            System.out.println("gojo");
+//        }
+//        System.out.println(!(a<b && b<c));
+//        Scanner sc = new Scanner(System.in);
+//        int age = sc.nextInt();
+//        switch (age) {
+//            case 18 -> System.out.println("Eligible");
+//            case 10 -> System.out.println("Not Eligible");
+//            default -> System.out.println("Enough");
+//        }
+//        for(int i=0; i<9; i++){
+//            if(i == 3) {
+//                System.out.println("Yes");
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+//        int n = 7;
+//        int temp=0;
+//        for(int i=2; i<(n-1); i++){
+//            if(n%i == 0){
+//                temp++;
+//            }
+//        }
+//        if(temp>1){
+//            System.out.println("Not prime");
+//        }else{
+//            System.out.println("Prime");
+//        }
+
+//        int[] marks = new int[5];
+//        marks[0] = 55;
+//        marks[1] = 65;
+//        marks[2] = 75;
+//        System.out.println(marks[2]);
+//        for(int i=0; i<marks.length; i++){
+//            System.out.println(marks[i]);
+//        }
+//        for(int element: marks){
+//            System.out.println(element);
+//        }
+//        for(int j=(marks.length-1); j>=0; j--){
+//            System.out.println(marks[j]);
+//        }
+
+//        int a=0,b=1;
+//        System.out.println(a);
+//        System.out.println(b);
+//        int c;
+//        for(int i=0; i<10; i++){
+//            c=a+b;
+//            System.out.println(c);
+//            a=b;
+//            b=c;
+//        }
+
+//        int a=5;
+//        int f=1;
+//        for(int i=1; i<=a; i++) {
+//            f *= i;
+//        }
+//            System.out.println(f);
+
+
+
+
+
+
+
+
+    }
+}
