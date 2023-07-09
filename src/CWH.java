70f052d3e557a6e634f72e6d3940490ca8de70d7
Saksham <sakshamgupta1279@gmail.com>
2023-07-09 23:19:29 +0530

    Just practice ques

---
+++ src/CWH.java
@@ -0,0 +1,192 @@
+import java.util.Scanner;
+public class CWH {
+
+//    static void Mutli(int a){
+//        for(int i=1; i<=10; i++) {
+//            System.out.println(a+"X"+i+"="+a*i);
+//        }
+//    }
+
+//    static void Pattern(int n){
+//        for(int i=1; i<=n; i++){
+//            for(int j=1; j<=i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+
+//    static int SUM(int n){
+//        if(n==1){
+//            return 1;
+//        }else{
+//            return n + SUM(n-1);
+//        }
+//    }
+
+//    static int fib(int n){
+//        if(n==1){
+//            return 0;
+//        }else if(n==2){
+//            return 1;
+//        }else {
+//            return fib(n-1) + fib(n-2);
+//        }
+//    }
+
+//    static int varags(int ...arr){
+//        int sum = 0;
+//        for(int e: arr){
+//            sum += e;
+//        }
+//        return sum/arr.length;
+//    }
+
+//    static void patt(int n){
+//        if(n>0){
+//            patt(n-1);
+//            for(int i=1; i<=n; i++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+
+//        Mutli(8);
+//        Pattern(4);
+//        int c = SUM(3);
+//        System.out.println(c);
+//        int c = fib(5);
+//        System.out.println(c);
+//        System.out.println(varags(3,8,1));
+//        patt(3);
+
+
+//        int i=0;
+//        int sum=0;
+//        while(i<6){
+//            System.out.println(2*i);
+//            sum = sum + (2*i);
+//            i++;
+//        }
+//        System.out.println("sum:"+sum);
+
+//        int n=5;
+//        for(int i=1; i<=10; i++){
+//            System.out.println(n+"x"+i+"="+n*i);
+//        }
+
+//        int n=10;
+//        for(int i=10; i>=0; i--){
+//            System.out.println(n+"x"+i+"="+n*i);
+//        }
+
+//        int sum=0;
+//        for(int i=1; i<=10; i++){
+//            System.out.println(i*8);
+//            sum += (i*8);
+//        }
+//        System.out.println("sum:"+ sum);
+
+//        float arr[] = {2,3,9,8,1};
+//        float sum = 0;
+//        for(float el: arr){
+//            System.out.println(el);
+//            sum += el;
+//        }
+//        System.out.println("sum:"+sum);
+
+//        int []marks = {55,22,99,88};
+//        int n=55;
+//        boolean InArray = false;
+//        for(int el: marks){
+//            if(n==el){
+//                InArray = true;
+//                break;
+//            }
+//        }
+//        if(InArray){
+//            System.out.println("Present");
+//        }else{
+//            System.out.println("Not Present");
+//        }
+
+//        int []marks = new int[2];
+//        marks[0] = 88;
+//        marks[1] = 22;
+//        int sum=0;
+//        for(int el: marks){
+//            System.out.println(el);
+//            sum = sum +el;
+//        }
+//        System.out.println("average:"+sum/2);
+
+//        int [][]mat1 = {{1,2,3},{4,5,6}};
+//        int [][]mat2 = {{11,23,33},{44,55,62}};
+//        int [][]result = {{0,0,0},{0,0,0}};
+//
+//        for(int i=0; i<mat1.length; i++){
+//            for(int j=0; j<mat1[i].length; j++){
+//                result[i][j] = mat1[i][j] + mat2[i][j];
+//            }
+//        }
+//        for(int i=0; i<mat1.length; i++){
+//            for(int j=0; j<mat1[i].length; j++){
+//                System.out.print(result[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        int []arr = {3,5,6,2};
+//        for(int i=arr.length-1; i>=0; i--){
+//            System.out.println(arr[i]);
+//        }
+
+//        int []arr = {1,2,3,4,5,6};
+//        int max = Integer.MIN_VALUE;
+//        int min = Integer.MAX_VALUE;
+//        for(int e:arr){
+//            if(e>max){
+//                max=e;
+//            }
+//        }
+//        System.out.println("Max:"+ max);
+//        for(int el: arr){
+//            if(el<min){
+//                min=el;
+//            }
+//        }
+//        System.out.println("Min:"+min);
+
+//        int []arr = {2,4,7,3};
+//        boolean IsSorted = true;
+//        for(int i=0; i<arr.length; i++){
+//            if(arr[i]> arr[i+1]){
+//                IsSorted = false;
+//                break;
+//            }
+//        }
+//        if(IsSorted){
+//            System.out.println("Sorted");
+//        }else{
+//            System.out.println("Not Sorted");
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
