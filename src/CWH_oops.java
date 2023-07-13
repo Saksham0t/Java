15ea80d6c4e96cf3ca472236293afd208c4e7667
Saksham <sakshamgupta1279@gmail.com>
2023-07-14 03:44:16 +0530

    Multiple Classes

---
+++ src/CWH_oops.java
@@ -0,0 +1,99 @@
+//class Employee{
+//    int salary;
+//    String name;
+//
+//    public int getSalary(){
+//        return salary;
+//    }
+//    public String getName(){
+//        return name;
+//    }
+//    public String setName(String n){
+//        return name=n;
+//    }
+//}
+
+//class Cellphone{
+//    public void Ringing(){
+//        System.out.println("Ringing");
+//    }
+//    public void Vibrating(){
+//        System.out.println("Vibrating");
+//    }
+//}
+
+//class Square{
+//    int side;
+//    public int area(){
+//        return side*side;
+//    }
+//    public int perimeter(){
+//        return 4*side;
+//    }
+//}
+
+//class Cylinder{
+//    private int radius;
+//    private int height;
+//
+//    Cylinder(int radius, int height) {
+//        this.radius = radius;
+//        this.height = height;
+//    }
+//
+//    public int getRadius(){
+//        return radius;
+//    }
+//    public void setRadius(int radius){
+//        this.radius = radius;
+//    }
+//    public int getHeight(){
+//        return height;
+//    }
+//    public void setHeight(int height){
+//        this.height = height;
+//    }
+//    public double volume(){
+//        return Math.PI*radius*radius;
+//    }
+//}
+
+//class Rectangle{
+//    Rectangle(int l , int b){
+//        System.out.println(l*b);
+//    }
+//}
+
+
+public class CWH_oops {
+    public static void main(String[] args) {
+
+//        Employee obj = new Employee();
+//        obj.salary = 222;
+//        obj.setName("Saksham");
+//        System.out.println(obj.getName());
+//        System.out.println(obj.getSalary());
+
+//        Cellphone realme = new Cellphone();
+//        realme.Ringing();
+//        realme.Vibrating();
+
+//        Square dbaa = new Square();
+//        dbaa.side = 8;
+//        System.out.println(dbaa.area());
+//        System.out.println(dbaa.perimeter());
+
+//        Cylinder cyl = new Cylinder();
+//        cyl.setHeight(8);
+//        cyl.setRadius(5);
+//        System.out.println(cyl.getRadius());
+//        System.out.println(cyl.getHeight());
+//        System.out.println(cyl.volume());
+
+//        Rectangle rec = new Rectangle(3,8);
+//        System.out.println(rec.a);
+
+
+
+    }
+}
