5f4afb61cdfade2304b65a8374b3b66b4b3af65e
Saksham <sakshamgupta1279@gmail.com>
2023-07-14 03:41:23 +0530

    Basic Inheritance

---
+++ src/Inheritance.java
@@ -0,0 +1,80 @@
+//class Circle{
+//    public int radius;
+//    Circle(){
+//        System.out.println("Simple Constructor");
+//    }
+//    Circle(int r){
+//        System.out.println("Parameterized Constructor");
+//        this.radius = r;
+//    }
+//    public double area(){
+//        return Math.PI*this.radius*this.radius;
+//    }
+//}
+//class Cyclinder extends Circle{
+//    public int height;
+//    Cyclinder(int r, int h){
+//        super(r);
+//        System.out.println("Cylinder Constructor");
+//        this.height = h;
+//    }
+//    public double volume(){
+//        return Math.PI*this.radius*this.radius*this.height;
+//    }
+//}
+
+//class Rectangle{
+//     int length;
+//     int breadth;
+//    public int getLength(){
+//        return length;
+//    }
+//    public void setLength(int length){
+//        this.length =length;
+//    }
+//    public int getBreadth() {
+//        return breadth;
+//    }
+//    public void setBreadth(int breadth) {
+//        this.breadth = breadth;
+//    }
+//    Rectangle(int l , int b){
+//        this.length = l;
+//        this.breadth = b;
+//    }
+//    public int area (){
+//        return this.length * this.breadth;
+//    }
+//}
+//class Cuboid extends Rectangle{
+//    private int height;
+//    public int getHeight() {
+//        return height;
+//    }
+//    public void setHeight(int height) {
+//        this.height = height;
+//    }
+//    Cuboid(int l, int b , int h){
+//        super(l,b);
+//        this.height = h;
+//    }
+//    public int volume(){
+//        return this.length*this.breadth*this.height;
+//    }
+//}
+
+public class Inheritance {
+    public static void main(String[] args) {
+//        Cyclinder cy = new Cyclinder(4,8);
+//        System.out.println(cy.area());
+//        System.out.println(cy.volume());
+
+//        Cuboid rec = new Cuboid(2,4,6);
+//        System.out.println(rec.area());
+//        System.out.println(rec.volume());
+
+
+
+
+    }
+}
