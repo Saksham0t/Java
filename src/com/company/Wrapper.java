f4bf09be70ec1d679ab222d78acccd08a2894aa4
Saksham Gupta <sakshamgupta1279@gmail.com>
2024-04-30 03:37:45 +0530

    updated wrapper code

--- src/com/company/Wrapper.java
+++ src/com/company/Wrapper.java
@@ -2,29 +2,15 @@ package com.company;
 public class Wrapper {
     public static void main(String[] args) {
 
-//        int i = 5;
-//        Integer ii = new Integer(i);     //Boxing
-//        Integer jj = i;                 //Auto Boxing
-//
+        int i = 5;
+        Integer jj = i;                 //Auto Boxing
+        System.out.println(jj);
+
 //        int j = jj.intValue();         //Unboxing
-//        int k= jj;                     //Auto Unboxing
+        int k= jj;                     //Auto Unboxing
 //
-//        System.out.println(k);
-//        System.out.println(j);
-
-          for (int i = 1; i<=5; i++){
-              for(int j=1; j<i; j++){
-                  System.out.print("*");
-              }
-              System.out.println();
-          }
-
-
-
-
-
-
-
+        System.out.println(k);
+        System.out.println(jj);
 
 
    }
